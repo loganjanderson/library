d07a83cde599c6ce300df3e4c58b33fc07ac570f
Logan Anderson <logananderson@LJA-MacBook-Air.local>
2025-04-11 13:09:40 -0600

    undoes changes to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles/styles.scss";
+
+import Home from "./components/home";
+import Header from "./components/header";
+import BookList from "./components/bookList";
+import AddBook from "./components/addBook";
 import Scanner from "./components/scanner";
 
-const App = () => {
+function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Scanner />} />
-        {/* Add other routes here */}
-      </Routes>
-    </div>
+    <Router>
+      <main>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<BookList />} />
+          <Route path="/addbook" element={<AddBook />} />
+          <Route path="/checkout" element={<Scanner />} />
+        </Routes>
+      </main>
+    </Router>
   );
-};
+}
 
 export default App;
